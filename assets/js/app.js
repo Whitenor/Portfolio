ddc115344c56a8815dc4a91adb760de0556522ce
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-06-29 11:14:37 +0200

    switch svg into html , add animate rotation for cross on click

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+document.querySelector('#folderSvg').addEventListener('click', function() {
+    document.querySelector('#crossFolder').classList.toggle('crossRotate');
+});
+document.querySelector('#homeSvg').addEventListener('click', function() {
+    document.querySelector('#crossHome').classList.toggle('crossRotate');
+});
