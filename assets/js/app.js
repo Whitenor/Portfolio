a33263b123fda89ddcc8d16be051bd8d9381ca33
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-06-29 12:54:06 +0200

    sidemenuhome start

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
-document.querySelector('#folderSvg').addEventListener('click', function() {
-    document.querySelector('#crossFolder').classList.toggle('crossRotate');
-});
 document.querySelector('#homeSvg').addEventListener('click', function() {
     document.querySelector('#crossHome').classList.toggle('crossRotate');
+    document.querySelector('.sideMenuHome').classList.toggle('hide');
+});
+document.querySelector('#folderSvg').addEventListener('click', function() {
+    document.querySelector('#crossFolder').classList.toggle('crossRotate');
 });
