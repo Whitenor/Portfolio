d1f3acbe8e59330b2524d45e88ce1e7b7b0e6525
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-06-30 10:57:27 +0200

    finish sidemenu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,52 @@
+var sideMenuFolder = document.querySelector('.sideMenuFolder');
+var sideMenuHome = document.querySelector('.sideMenuHome');
+var crossHome = document.querySelector('#crossHome');
+var crossFolder = document.querySelector('#crossFolder');
+var borderChecking = 0;
+var openChecking = 0;
+
+function checkBorder(target){
+    for (let i = 0; i < target.classList.length; i++) {
+        if(target.classList[i] === 'borderNone'){
+            borderChecking = 1
+        }
+    }
+    if(borderChecking === 0){
+        setTimeout(() => {
+            target.classList.toggle('borderNone');
+        }, 500);
+    }
+    else{
+        target.classList.toggle('borderNone');
+        borderChecking = 0;
+    }
+}
+function checkOtherOpen(firstTarget, crossTarget ){
+    for (let i = 0; i < firstTarget.classList.length; i++) {
+        if(firstTarget.classList[i] === 'open'){
+            openChecking = 1;
+        }
+    }
+    console.log(openChecking);
+    if (openChecking === 1) {
+        firstTarget.classList.toggle('open');
+        firstTarget.classList.toggle('hide');
+        crossTarget.classList.toggle('crossRotate');
+        openChecking = 0;
+        checkBorder(firstTarget);
+    }
+}
 document.querySelector('#homeSvg').addEventListener('click', function() {
-    document.querySelector('#crossHome').classList.toggle('crossRotate');
-    document.querySelector('.sideMenuHome').classList.toggle('hide');
+    crossHome.classList.toggle('crossRotate');
+    sideMenuHome.classList.toggle('hide');
+    sideMenuHome.classList.toggle('open');
+    checkOtherOpen(sideMenuFolder, crossFolder);
+    checkBorder(sideMenuHome);
 });
 document.querySelector('#folderSvg').addEventListener('click', function() {
-    document.querySelector('#crossFolder').classList.toggle('crossRotate');
+    crossFolder.classList.toggle('crossRotate');
+    sideMenuFolder.classList.toggle('hide');
+    sideMenuFolder.classList.toggle('open');
+    checkOtherOpen(sideMenuHome, crossHome);
+    checkBorder(sideMenuFolder);
 });
